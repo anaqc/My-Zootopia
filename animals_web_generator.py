8de72284b2199c597409c276ead0e6f295d064fc
elisaquispe2011 <elisaquispe2011@gmail.com>
2025-01-05 00:24:33 +0100

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -6,17 +6,36 @@ def load_data(file_path):
     with open(file_path, "r") as handle:
         return json.load(handle)
 
+def read_animals_html(file_path):
+    with open(file_path, "r") as handle:
+        return handle.read()
+
+def print_animals_info(animals):
+    output = ""
+    for animal in animals:
+        animal_type = animal["characteristics"].get("type")
+        output += f"Name: {animal['name']}\n"
+        output += f"Diet: {animal['characteristics']['diet']}\n"
+        output += f"First location: {animal["locations"][0]}\n"
+        if animal_type is not None:
+            output += f"Type: {animal_type}\n"
+        output += f"\n"
+    return output
+
+def write_new_content(file_path, content):
+    with open(file_path, "w") as f:
+        f.write(content)
 
 def main():
     animals_data = load_data("animals_data.json")
-    for animal in animals_data:
-        animal_type = animal["characteristics"].get("type")
-        print(f"Name: {animal["name"]}")
-        print(f"Diet: {animal["characteristics"]["diet"]}")
-        print(f"First location: {animal["locations"][0]}")
-        if animal_type is not None:
-            print(f"Type: {animal_type}")
-        print()
+    html_content = read_animals_html("animals_template.html")
+
+    new_html_content = html_content.replace("__REPLACE_ANIMALS_INFO__",
+                                            print_animals_info(animals_data))
+    write_new_content("animals.html", new_html_content)
+
+
+
 
 
 if __name__ == "__main__":
